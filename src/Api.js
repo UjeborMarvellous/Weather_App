7b42f7e0eb4d93d0b4f8f518ee256c34d1dc6f2a
UjeborMarvellous <92124127+UjeborMarvellous@users.noreply.github.com>
2023-04-13 22:30:17 +0100

    Weather_App

---
+++ src/Api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3388e8ac4bmsh7e2b7d4ce5db89bp1da1a9jsn8989d5525a8d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "2e06b3c99688938957b3bc44ffe7f6e4"
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+/* eslint-disable no-undef */
 import './App.css';
+import Currentweather from './Component/Current/Current-weather';
+import Search from './Component/Search/Search';
+import { WEATHER_API_URL, WEATHER_API_KEY } from './Api'
+import { useState } from 'react';
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  
+  const handleOnSearchChange = (searchData) => {
+    const [ lat, lon ] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+    .then(async (response) => {
+      const weatherResponse = await response[0].json();
+
+      setCurrentWeather({ city: searchData.label , ...weatherResponse});
+      setForecast({ city: searchData.label, ...forecastResponse})
+    })
+    .catch((err) => console.log(err));
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <Search 
+      onSearchChange={handleOnSearchChange}
+      />
+      <h1 className='H1'>WELCOME, Stay prepared with our accurate weather updates</h1>
+      {currentWeather && <Currentweather data={currentWeather}/>}
+
     </div>
   );
 }
